8842a9a9f5ab025e2e86b5c8893d1374aa769da5
mingi1123 <min_gi1123@naver.com>
2023-06-16 17:38:23 +0900

    fix: add MainServer bind listen

--- SimplePoW/MainServer.c
+++ SimplePoW/MainServer.c
@@ -32,9 +32,56 @@ void handleWorkerResults(int* workers, int numWorkers) {
 int performPoW(const char* challenge) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     int nonce = 0;
-    //...
+
+    while (1) {
+        // challenge와 nonce를 합쳐서 입력 데이터 생성
+        char input[CHALLENGE_SIZE + sizeof(int)];
+        snprintf(input, CHALLENGE_SIZE + sizeof(int), "%s%d", challenge, nonce);
+
+        // SHA256 해시 계산
+        SHA256((unsigned char*)input, strlen(input), hash);
+
+        // 해시 값의 난이도 확인
+        int difficultyCount = 0;
+        for (int i = 0; i < HASH_DIFFICULTY / 2; i++) {
+            if (hash[i] == 0) {
+                difficultyCount++;
+            }
+        }
+
+        // 난이도가 충족되면 작업 완료
+        if (difficultyCount >= HASH_DIFFICULTY / 2) {
+            break;
+        }
+
+        nonce++;
+    }
+
+    return nonce;
+}
+
+void measurePoWTime() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    // Proof of Work 수행
+    char challenge[CHALLENGE_SIZE];
+    snprintf(challenge, CHALLENGE_SIZE, "학번||학번||학번||이름||이름||이름");
+    int nonce = performPoW(challenge);
+    printf("Proof of Work complete. Nonce: %d\n", nonce);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("PoW Time: %.6f seconds\n", cpu_time_used);
+}
 
 int main() {
+    
+    int workers[MAX_WORKERS];
+    int numWorkers = 0;
+
     struct sockaddr_in serverAddr, workerAddr;
     socklen_t addrLen;
     int listenFd, connFd;
@@ -46,7 +93,7 @@ int main() {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
-
+    
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
@@ -76,14 +123,23 @@ int main() {
         }
     }
 
-    // challenge 생성, 코드 동일
-    //...
+    // challenge 생성
+    char challenge[CHALLENGE_SIZE];
+    snprintf(challenge, CHALLENGE_SIZE, "학번||학번||학번||이름||이름||이름");
+
+    // distributeChallenge 함수를 통해 challenge 값 배포
+    distributeChallenge(challenge, workers, numWorkers);
+
+    // handleWorkerResults 함수를 통해 Working Server들의 결과 확인
+    handleWorkerResults(workers, numWorkers);
+
+    // Proof of Work 시간 측정
+    measurePoWTime();
 
     // 연결 종료
     for (int i = 0; i < numWorkers; i++) {
         close(workers[i]);
     }
-    close(listenFd);
 
     return 0;
 }
